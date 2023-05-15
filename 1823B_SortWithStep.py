f5bb283cf74c6845f298e5a2d97381491e853836
汪志鹏 <13476859295@163.com>
2023-05-15 22:31:03 +0800

    finished 1823B

--- 1823B_SortWithStep.py
+++ 1823B_SortWithStep.py
@@ -1,54 +1,15 @@
-def can_sort_permutation(test_cases):
-    results = []
-    
-    for n, k, p in test_cases:
-        sorted_indices = sorted(range(n), key=lambda x: p[x])
-        possible = True
-        for i in range(n):
-            if (sorted_indices[i] - i) % k != 0:
-                possible = False
-                break
-
-        if possible:
-            results.append(0)
-        else:
-            preliminary_exchange = False
-            for i in range(n - 1):
-                if p[i] > p[i + 1]:
-                    p[i], p[i + 1] = p[i + 1], p[i]
-                    preliminary_exchange = True
-                    break
-            sorted_indices = sorted(range(n), key=lambda x: p[x])
-            possible = True
-            for i in range(n):
-                if (sorted_indices[i] - i) % k != 0:
-                    possible = False
-                    break
-                    
-            if possible:
-                results.append(1 if preliminary_exchange else 0)
-            else:
-                results.append(-1)
-
-    return results
-
-# Example usage:
-#t = 6
-#test_cases = [
-#    (4, 1, [3, 1, 2, 4]),
-#    (4, 2, [3, 4, 1, 2]),
-#    (4, 2, [3, 1, 4, 2]),
-#    (10, 3, [4, 5, 9, 1, 8, 6, 10, 2, 3, 7]),
-#    (10, 3, [4, 6, 9, 1, 8, 5, 10, 2, 3, 7]),
-#    (10, 3, [4, 6, 9, 1, 8, 5, 10, 3, 2, 7]),
-#]
-
-n = input()
-test_cases = []
-for i in range(int(n)):
-    t = [int(num) for num in input().split(' ')]
-    temp = [int(num) for num in input().split(' ')]
-    t.append(temp)
-    test_cases.append(tuple(t))
-
-print(*can_sort_permutation(test_cases))
+for tt in range(int(input())):
+    n, k = map(int, input().split(' '))
+    p = list(map(int, input().split(' ')))
+    for i in range(0, n):
+        p[i] -= 1
+    bad = 0
+    for i in range(0, n):
+        if p[i] % k != i % k:
+            bad += 1
+    if bad == 0:
+        print(0)
+    elif bad == 2:
+        print(1)
+    else:
+        print(-1)
