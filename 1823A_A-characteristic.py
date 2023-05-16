0fdf0e7346c292a370c20bde3dbddf3ac9b49da5
汪志鹏 <13476859295@163.com>
2023-05-16 09:27:07 +0800

    Update 1823A_A-characteristic.py

--- 1823A_A-characteristic.py
+++ 1823A_A-characteristic.py
@@ -1,2 +1,15 @@
+def fun(n):
+    return n*(n-1)/2
+
 for _ in range(int(input())):
     n, k = map(int, input().split(' '))
+    hasFind = False
+    for i in range(n+1):
+        if k == fun(i) + fun(n-i):
+            print('Yes')
+            hasFind = True
+            a = [1 for _ in range(i)] + [-1 for _ in range(n-i)]
+            print(*a)
+            break
+    if not hasFind:
+        print('No')
