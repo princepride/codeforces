ab554b13859af8045124e7f7243c7a9635255e46
汪志鹏 <13476859295@163.com>
2023-05-19 23:01:27 +0800

    Update 1819A_ConstructiveProblem.py

--- 1819A_ConstructiveProblem.py
+++ 1819A_ConstructiveProblem.py
@@ -1,39 +1,56 @@
 
-def find_missing_number(arr):
-    # 将数组转换为集合
-    num_set = set(arr)
+#def find_missing_number(arr):
+#    # 将数组转换为集合
+#    num_set = set(arr)
 
-    # 从1开始逐个增加数字，检查是否在集合中出现
-    num = 0
-    while num in num_set:
-        num += 1
+#    # 从1开始逐个增加数字，检查是否在集合中出现
+#    num = 0
+#    while num in num_set:
+#        num += 1
 
-    return num
+#    return num
 
-for _ in range(int(input())):
+#for _ in range(int(input())):
+#    n = int(input())
+#    arr = list(map(int, input().split(' ')))
+#    m = find_missing_number(arr)
+#    min_index = -1
+#    max_index = -1
+#    for index, num in enumerate(arr):
+#        if num == m + 1:
+#            if min_index == -1:
+#                min_index = index
+#            else:
+#                max_index = index
+#    if min_index == -1:
+#        if len(arr) == len(set(arr)):
+#            print('no')
+#        else:
+#            print('yes')
+#    elif max_index == -1:
+#        print('yes')
+#    else:
+#        for i in range(min_index,max_index+1):
+#            arr[i] = m
+#        if find_missing_number(arr) == m+1:
+#            print('yes')
+#        else:
+#            print('no')
+
+def solve():
     n = int(input())
-    arr = list(map(int, input().split(' ')))
-    m = find_missing_number(arr)
-    min_index = -1
-    max_index = -1
-    for index, num in enumerate(arr):
-        if num == m + 1:
-            if min_index == -1:
-                min_index = index
-            else:
-                max_index = index
-    if min_index == -1:
-        if len(arr) == len(set(arr)):
-            print('no')
-        else:
-            print('yes')
-    elif max_index == -1:
-        print('yes')
+    a = list(map(int, input().split()))
+    mex = 0
+    while mex in a:
+        mex += 1
+    if mex == n:
+        print('YES')
     else:
-        for i in range(min_index,max_index+1):
-            arr[i] = m
-        if find_missing_number(arr) == m+1:
-            print('yes')
+        if mex in a[mex:]:
+            print('NO')
         else:
-            print('no')
-
+            print('YES')
+            
+t = int(input())
+for _ in range(t):
+    solve()
